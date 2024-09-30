ccdd221146e6a9266b32bd84498710a8e1fa0477
Meina Ghafourivarzaneh <meinsta@gmail.com>
2024-09-29 17:17:13 -0700

    reconciling files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SignUpPage from './components/SignUpPage';
+import TermsOfService from './components/TermsOfService';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUpPage() {
+  return (
+    <div className="sign-up-page">
+      <h1>Sign Up for Grounded Affirmations</h1>
+      {/* Add your sign-up form here */}
+    </div>
+  );
+}
+
+export default SignUpPage;
