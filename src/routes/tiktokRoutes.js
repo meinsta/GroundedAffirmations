314b2230558e94d2c80389bc91a6d18e0a94c76a
Meina Ghafourivarzaneh <meinsta@gmail.com>
2024-10-04 12:48:12 -0700

    Updates

--- src/routes/tiktokRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tiktokController = require('../controllers/tiktokController');
-
-router.get('/auth', tiktokController.getAuthUrl);
-router.get('/callback', tiktokController.handleCallback); // Make sure this line is present
-
-module.exports = router;
